835512551bfb5078e29c52499fcf8ebfbe92d808
Leticia Cardoso Gama <letcgam@corp.cepetro.unicamp.br>
2024-05-02 17:50:44 -0300

    order form and models

--- delivery/models.py
+++ delivery/models.py
@@ -135,7 +135,7 @@ class Payment(models.Model):
 
 
 class Recipient(models.Model):
-    id = models.AutoField(primary_key=True)pronto para ser retirado
+    id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
